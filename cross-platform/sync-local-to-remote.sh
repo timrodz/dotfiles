eb7d38b1c7d1d5d7de09fb48ae4b4998b96ba43f
Juan Morais <timrodz@icloud.com>
2022-08-09 14:09:23 +1200

    sync regolith overrides in shell scripts

--- cross-platform/sync-local-to-remote.sh
+++ cross-platform/sync-local-to-remote.sh
@@ -5,5 +5,6 @@ echo "synchronizing settings (local to remote)"
 cp $HOME/.zshrc .zshrc
 cp $HOME/.tmux.conf .tmux.conf
 cp $HOME/.alacritty.yml .alacritty.yml
+cp $HOME/.config/regolith/Xresources .config/regolith/Xresources
 
 echo "done!"
--- cross-platform/sync-remote-to-local.sh
+++ cross-platform/sync-remote-to-local.sh
@@ -5,5 +5,6 @@ echo "synchronizing settings (remote to local)"
 cp .zshrc $HOME/.zshrc
 cp .tmux.conf $HOME/.tmux.conf
 cp .alacritty.yml $HOME/.alacritty.yml
+cp .config/regolith/Xresources $HOME/.config/regolith/Xresources
 
 echo "done!"

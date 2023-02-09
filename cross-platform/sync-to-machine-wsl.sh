0f19635bb92255ab2cc09b6c5b236681501ea242
Juan Morais <timrodz@icloud.com>
2023-02-09 17:10:33 +1300

    feat: Add WSL helpers

--- cross-platform/sync-to-machine-wsl.sh
+++ cross-platform/sync-to-machine-wsl.sh
@@ -5,5 +5,6 @@ echo "synchronizing settings (remote to local)"
 cp .zshrc-wsl $HOME/.zshrc
 cp .tmux.conf.local $HOME/.tmux.conf.local
 cp .alacritty.yml $HOME/.alacritty.yml
+sudo cp wsl.conf /etc/wsl.conf
 
 echo "done!"

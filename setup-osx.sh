23e1ef5365caba285ba36dde67726f0336838ce7
Juan Morais <timrodz@icloud.com>
2024-11-07 09:35:21 +1300

    refactor: Split OSX + Linux configs

--- setup-osx.sh
+++ setup-osx.sh
@@ -29,7 +29,34 @@ echo "\n> Terminal fun: Installing Alacritty + Tmux + NeoVim + FZF + Zoxide"
 
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 brew install --cask alacritty
-brew install neovim fzf zoxide tmux asdf
+brew install neovim fzf zoxide tmux asdf duti
+
+echo "\n> Setting default apps to be VScode"
+duti -s com.microsoft.VSCode .c all
+duti -s com.microsoft.VSCode .cpp all
+duti -s com.microsoft.VSCode .cs all
+duti -s com.microsoft.VSCode .css all
+duti -s com.microsoft.VSCode .go all
+duti -s com.microsoft.VSCode .java all
+duti -s com.microsoft.VSCode .js all
+duti -s com.microsoft.VSCode .sass all
+duti -s com.microsoft.VSCode .scss all
+duti -s com.microsoft.VSCode .less all
+duti -s com.microsoft.VSCode .vue all
+duti -s com.microsoft.VSCode .cfg all
+duti -s com.microsoft.VSCode .json all
+duti -s com.microsoft.VSCode .jsx all
+duti -s com.microsoft.VSCode .lua all
+duti -s com.microsoft.VSCode .md all
+duti -s com.microsoft.VSCode .php all
+duti -s com.microsoft.VSCode .pl all
+duti -s com.microsoft.VSCode .py all
+duti -s com.microsoft.VSCode .rb all
+duti -s com.microsoft.VSCode .rs all
+duti -s com.microsoft.VSCode .sh all
+duti -s com.microsoft.VSCode .swift all
+duti -s com.microsoft.VSCode .txt all
+duti -s com.microsoft.VSCode .conf all
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 git config --global core.editor nvim

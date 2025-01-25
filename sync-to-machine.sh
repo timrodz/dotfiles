f647b7258aee4ac6d07c4b12cfe4becd4dcc5980
Juan Morais <timrodz@icloud.com>
2025-01-25 22:33:03 +1300

    feat: Add .tool-versions for asdf

--- sync-to-machine.sh
+++ sync-to-machine.sh
@@ -6,6 +6,7 @@ cp config/zshrc $HOME/.zshrc
 cp config/tmux.conf $HOME/.config/tmux/tmux.conf
 cp config/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 cp config/ohmyposh.toml $HOME/.config/ohmyposh/config.toml
+cp config/.tool-versions $HOME/.tool-versions
 
 cd $HOME
 exec zsh
